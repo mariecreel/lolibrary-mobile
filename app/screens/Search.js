6a675768e8f54f7c03e6422c938e5e7de5190f83
mariecreel <nick.creel@protonmail.com>
2022-01-12 17:35:16 -0500

    make request to lolibrary search API

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,17 +1,40 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
-import SearchBar from '../components/SearchBar';
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const Search = () => {
-    const baseSearchUrl = '';
-    const [searchText, setSearchText] = useState('');
-    const [searchData, setSearchData] = useState([]);
-    return ( 
-        <View>
-            <SearchBar onChangeText={setSearchText}/>
-            <Text>{searchText ? `Search results for ${searchText}:` : "Search results will appear below."}</Text>
-        </View>
-    );
-}
+  const baseSearchUrl = "https://lolibrary.org/api/search?=";
+  const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState(null);
+  const [total, setTotal] = useState(0);
+
+  // search automatically when text in input changes
+  // wait three seconds until user has stopped typing
+  useEffect(() => {
+    if (searchTerm) {
+      setTimeout(() => {
+        fetch(`${baseSearchUrl}${searchTerm}`, { method: 'POST' })
+          .then((res) => res.json())
+          .then((data) => {
+            setData(data);
+            setTotal(data.total);
+          })
+          .catch((e) => console.error(e));
+      }, 3000);
+    }
+  }, [searchTerm]);
+
+  return (
+    <View>
+      <SearchBar onChangeText={setSearchTerm} text={searchTerm} />
+      <Text>
+        {searchTerm
+          ? `Search results for ${searchTerm} (${total} total results):`
+          : "Search results will appear below."}
+      </Text>
+      <Text>{data ? data.toString() : ''}</Text>
+    </View>
+  );
+};
 
 export default Search;
