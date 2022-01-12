87ea75dd9eadcabd7fcbd47ce3669356f93dd702
mariecreel <nick.creel@protonmail.com>
2022-01-12 18:47:39 -0500

    search for keyword and display list of search results

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import SearchBar from "../components/SearchBar";
+import SearchResults from "../components/SearchResults";
 
 const Search = () => {
-  const baseSearchUrl = "https://lolibrary.org/api/search?=";
+  const baseSearchUrl = "https://lolibrary.org/api/search";
   const [searchTerm, setSearchTerm] = useState("");
   const [data, setData] = useState(null);
   const [total, setTotal] = useState(0);
@@ -11,17 +12,34 @@ const Search = () => {
   // search automatically when text in input changes
   // wait three seconds until user has stopped typing
   useEffect(() => {
-    if (searchTerm) {
-      setTimeout(() => {
-        fetch(`${baseSearchUrl}${searchTerm}`, { method: 'POST' })
-          .then((res) => res.json())
-          .then((data) => {
-            setData(data);
-            setTotal(data.total);
-          })
-          .catch((e) => console.error(e));
-      }, 3000);
-    }
+      const fetchSearchData = setTimeout(() => {
+            if (searchTerm) {
+                fetch(`${baseSearchUrl}`, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ 
+                    brands: [],
+                    categories: [],
+                    colors: [],
+                    features: [],
+                    page: 1,
+                    tags: [],
+                    years: [],
+                    search: searchTerm 
+                    }),
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    setData(data);
+                    setTotal(data.total);
+                })
+                .catch((e) => console.error(e));
+            }
+        }, 3000);
+        // need to clear timeout to prevent fetching data prematurely
+        // before user has completed typing
+        // see https://stackoverflow.com/a/61629055
+        return () => clearTimeout(fetchSearchData);
   }, [searchTerm]);
 
   return (
@@ -32,7 +50,7 @@ const Search = () => {
           ? `Search results for ${searchTerm} (${total} total results):`
           : "Search results will appear below."}
       </Text>
-      <Text>{data ? data.toString() : ''}</Text>
+      {data ? <SearchResults data={data.data} /> : <></>}
     </View>
   );
 };
