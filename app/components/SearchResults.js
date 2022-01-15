94be59244d1ee598368cebe3d11fdc94e896cf56
mariecreel <nick.creel@protonmail.com>
2022-01-15 16:47:29 -0500

    load next page of results if available when at end of search results

--- app/components/SearchResults.js
+++ app/components/SearchResults.js
@@ -1,25 +1,64 @@
-import React, {useState, useEffect} from 'react';
-import {View, FlatList} from 'react-native';
-import SearchResult from './SearchResult';
+import React, { useState, useEffect, useRef } from "react";
+import { View, FlatList, StyleSheet} from "react-native";
+import SearchResult from "./SearchResult";
+import fetchSearchData from "../utility/fetchSearchData";
+import LoadingSpinner from "./LoadingSpinner";
 
-const SearchResults = ({data}) => {
-    const [page, setPage] = useState('1');
+const SearchResults = ({ data, searchTerm, total }) => {
+  const [page, setPage] = useState(1);
+  const [results, setResults] = useState(data.data);
+  const [loading, setLoading] = useState(false);
 
-    const _renderItem = ({item}) => {
-        return (
-            <SearchResult item={item}/>
-        )
+  // set mountedRef.current = true on mount so that 
+  // page state hook has no effect on mount.
+  // we need to set a variable on mount because useEffect
+  // automatically runs on mount.
+  // see https://stackoverflow.com/a/64194949
+  const mountedRef = useRef();
+  
+  useEffect(() => {
+    // only run after mount
+    if (mountedRef.current) {
+      Promise.resolve(fetchSearchData(searchTerm, page)).then((data) => {
+        setResults(results.concat(data.data));
+        setLoading(false);
+      });
     }
+  }, [page]);
 
-    return (
-        <View style={{height: '90%'}}>
-            <FlatList 
-                data={data}
-                renderItem={_renderItem}
-                keyExtractor={item => item.slug}
-            />
-        </View>
-    )
-}
+  useEffect(() => {
+    mountedRef.current = true;
+  }, []);
+
+  function _renderItem ({item}) {
+    return <SearchResult item={item} />;
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      <FlatList
+        data={results}
+        renderItem={_renderItem}
+        // sometimes slugs are not unique, so we add the index to force uniqueness
+        // unfortunately, lolibrary data does not have stable, unique ID
+        keyExtractor={(item, index) => item.slug + index.toString()}
+        onEndReached={() => {
+          if (page < data.last_page) {
+            setLoading(true);
+            setPage(page + 1);
+          }
+        }}
+      />
+      {loading ? <LoadingSpinner searching={loading}/> : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    wrapper: {
+        height: '90%',
+        alignItems: 'center',
+    }
+})
 
 export default SearchResults;
